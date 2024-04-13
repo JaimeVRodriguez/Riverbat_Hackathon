bd9478f371a0db4d8a881f923a570d9ff6201901
Johnathan Riemerspeters <johnathan.riemerspeters@swf.army.mil>
2024-04-13 14:13:16 -0500

    working frontend with typing of tracks objects, minor fix to MusicController apiKey

--- frontend/src/components/MusicPlayer.tsx
+++ frontend/src/components/MusicPlayer.tsx
@@ -1,36 +1,72 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
+type ArtistObject = {
+    mbid: string;
+    name: string;
+    url: string;
+};
+type ImageObject = {
+    '#text': string;
+    size: string;
+};
+type TrackObject = {
+    artist: ArtistObject;
+    duration: string;
+    image: ImageObject[];
+    mbid: string;
+    name: string;
+    streamable:{
+        '#text': string;
+        fullTrack: string;
+    };
+    url: string;
+}
 const MusicPlayer: React.FC = () => {
     const [tracks, setTracks] = useState([]);
+    const [selectedDecade, setSelectedDecade] = useState('70s');
+    const [currentTrack, setCurrentTrack] = useState('');
     useEffect(() => {
         const fetchMusicData = async () => {
             try {
-                const response =
-                    await axios.get('http://localhost:8080/music', );
+                const response = await axios.get(`http://localhost:8080/music/${selectedDecade}`);
+                console.log(response.data.tracks.track)
                 setTracks(response.data.tracks.track);
-            } catch
-                (error)
-                {
-                    console.error('Error fetching music data:', error);
-                }
+            } catch (error) {
+                console.error('Error fetching music data:', error);
             }
-            ;
-            fetchMusicData();
-        }, []
-    )
-        ;
-        return (
-            <div>
-                <h1>Retro Music Player</h1>
-                <ul>
-                    {tracks.map((track: any) => (
-                        <li key={track.name}>
-                            {track.name} - {track.artist.name}
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        );
+        };
+        fetchMusicData();
+    }, [selectedDecade]);
+    const playTrack = (trackUrl: string) => {
+        setCurrentTrack(trackUrl);
     };
-    export default MusicPlayer;
+    return (
+        <div>
+            <h1>Retro Music Player</h1>
+            <div>
+                <label htmlFor="decade">Select Decade:</label>
+                <select id="decade" value={selectedDecade} onChange={(e) => setSelectedDecade(e.target.value)}>
+                    <option value="70s">70s</option>
+                    <option value="80s">80s</option>
+                    <option value="90s">90s</option>
+                </select>
+            </div>
+            <ul>
+                {tracks.map((track: TrackObject) => (
+                    <li key={track.name}>
+                        {track.name} - {track.artist.name}
+                        <button onClick={() => playTrack(track.url)}>Play</button>
+                    </li>
+                ))}
+            </ul>
+            {currentTrack && (
+                <audio controls autoPlay>
+                    <source src={currentTrack} type="audio/mpeg"/>
+                    Your browser does not support the audio element.
+                </audio>
+            )}
+        </div>
+    );
+};
+export default MusicPlayer;
--- src/main/java/com/swf/hackathon/MusicController.java
+++ src/main/java/com/swf/hackathon/MusicController.java
@@ -1,22 +1,32 @@
 package com.swf.hackathon;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-
 @RestController
 @RequestMapping("/music")
 public class MusicController {
-    private String apiKey="9beb98cd8c38d5bd1eae3ae43bda26a8";
-    
-    @Value("${apiKey}")
+    private final  String apiKey="9beb98cd8c38d5bd1eae3ae43bda26a8";
+
+//    @Value("${apiKey}")
     @CrossOrigin
-    @GetMapping
-    public String getMusic() {
-        String url = "http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=" + apiKey + "&format=json&limit=10";
+    @GetMapping("/{decade}")
+    public String getMusicByDecade(@PathVariable String decade) {
+        String tag;
+        switch (decade) {
+            case "70s":
+                tag = "70s";
+                break;
+            case "80s":
+                tag = "80s";
+                break;
+            case "90s":
+                tag = "90s";
+                break;
+            default:
+                tag = "70s"; // Default to 70s if invalid decade provided
+        }
+        String url = "http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=" + tag + "&api_key=" + apiKey + "&format=json&limit=10";
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForObject(url, String.class);
     }
