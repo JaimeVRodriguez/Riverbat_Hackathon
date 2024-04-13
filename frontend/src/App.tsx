95a2692e9ba0bc6cbb938c5670ec3560bb2d0e2f
Johnathan Riemerspeters <johnathan.riemerspeters@swf.army.mil>
2024-04-13 12:59:06 -0500

    initial deviations, initial template, image not working at all

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Box, Button} from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <Box>
+                <Button>1</Button>
+                <Button>2</Button>
+                <Button>3</Button>
+                <Button>4</Button>
+                <Box component="img"
+                     sx={{
+                         height:233,
+                         width: 350,
+                     }}
+                     alt="Template image"
+                     src="template_image.jpg" />
+            </Box>
+        </>
+    )
 }
 
 export default App
