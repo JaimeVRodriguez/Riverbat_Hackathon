4961139589ca37563ce7f422fa5a51c8b1a03018
Jaime Rodriguez <jaime.v.rodriguez.mil@swf.army.mil>
2024-04-13 19:41:55 -0500

    Framework complete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,5 @@
 import './App.css'
-import {Box, Button, Stack} from "@mui/material";
-import {
-    PlayArrow,
-    SkipNextRounded,
-    SkipPreviousRounded
-} from "@mui/icons-material";
+import {Box, Stack, Typography} from "@mui/material";
 import MusicPlayer from "./components/MusicPlayer.tsx";
 
 function App() {
@@ -13,27 +8,13 @@ function App() {
         <>
             <Box>
                 <Stack>
-                    <Stack direction="row" justifyContent="center">
-                        <Button>1</Button>
-                        <Button>2</Button>
-                        <Button>3</Button>
-                        <Button>4</Button>
-                    </Stack>
+                    <Typography variant={'h1'}>Retro Music Player</Typography>
+                    <MusicPlayer/>
 
-                    <Box component="img"
-                         sx={{
-                             height: 233,
-                             width: 350,
-                         }}
-                         alt="Template image"
-                         src="template_image.jpg"/>
+
+
                 </Stack>
-                <Stack direction="row" justifyContent="center">
-                    <Button><SkipPreviousRounded/></Button>
-                    <Button><PlayArrow/></Button>
-                    <Button><SkipNextRounded/></Button>
-                </Stack>
-                <MusicPlayer/>
+
             </Box>
         </>
     )
--- frontend/src/components/MusicPlayer.tsx
+++ frontend/src/components/MusicPlayer.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
-import {Box} from "@mui/material";
+import {Box, Button, Chip, Stack, Typography} from "@mui/material";
+import {PlayArrow, SkipNextRounded, SkipPreviousRounded} from "@mui/icons-material";
 
 type ArtistObject = {
     mbid: string;
@@ -24,9 +25,9 @@ type TrackObject = {
     url: string;
 }
 const MusicPlayer: React.FC = () => {
-        const [tracks, setTracks] = useState([]);
+        const [tracks, setTracks] = useState<TrackObject[]>([]);
         const [selectedDecade, setSelectedDecade] = useState('70s');
-        const [currentTrack, setCurrentTrack] = useState('');
+        const [currentTrack, setCurrentTrack] = useState(0);
         const [currentYoutubeURL, setCurrentYoutubeURL] = useState('https://www.youtube.com/embed/EoVQ_TQFJy0');
         const youtubeRegex = "data-youtube-url=(.+)";
         const actualRegex = new RegExp(youtubeRegex, "g");
@@ -34,8 +35,6 @@ const MusicPlayer: React.FC = () => {
             const fetchMusicData = async () => {
                 try {
                     const response = await axios.get(`http://localhost:8080/music/${selectedDecade}`);
-                    console.log(response.data);
-                    console.log(response.data.tracks.track)
                     setTracks(response.data.tracks.track);
                 } catch (error) {
                     console.error('Error fetching music data:', error);
@@ -44,12 +43,17 @@ const MusicPlayer: React.FC = () => {
             fetchMusicData();
         }, [selectedDecade]);
 
+    useEffect(() => {
+        if(tracks.length>0) {
+            playTrack(tracks[currentTrack])
+        }
+    }, [currentTrack]);
+
         const fetchYoutubeURL = async (track: TrackObject) => {
             try {
                 const response = await axios.get(`http://localhost:8080/music/track/${btoa(track.url)}`);
                 let regexMatch = response.data.match(actualRegex)[0];
-                console.log(regexMatch);
-                regexMatch = regexMatch.slice(regexMatch.lastIndexOf('=')+1,-1);
+                regexMatch = regexMatch.slice(regexMatch.lastIndexOf('=') + 1, -1);
                 return regexMatch;
             } catch (error) {
                 console.error('Error fetching music data:', error);
@@ -58,49 +62,66 @@ const MusicPlayer: React.FC = () => {
 
         const playTrack = (track: TrackObject) => {
             fetchYoutubeURL(track).then(elmnt => {
-                    console.log(elmnt);
-                    setCurrentYoutubeURL('https://www.youtube.com/embed/' + elmnt+'?autoplay=1');
-                    setCurrentTrack(track.url);
+                    setCurrentYoutubeURL('https://www.youtube.com/embed/' + elmnt + '?autoplay=1');
+                    // setCurrentTrack(track);
                 }
             )
         };
+
+        const handleOnClick = (decade: string) => {
+            setSelectedDecade(decade)
+        }
+
+        function handlePlayClick() {
+            playTrack(tracks[currentTrack])
+        }
+
+        function handleNextClick() {
+            console.log(tracks[currentTrack].image[3]["#text"])
+            if(currentTrack + 1 === tracks.length) {
+                return setCurrentTrack(0);
+            }
+            setCurrentTrack(currentTrack + 1);
+        }
+
+
+        function handlePreviousClick() {
+            if(currentTrack - 1 < 0) {
+                return setCurrentTrack(tracks.length - 1);
+            }
+            setCurrentTrack(currentTrack - 1);
+
+        }
+
         return (
             <div>
-                <h1>Retro Music Player</h1>
-                <div>
-                    <label htmlFor="decade">Select Decade:</label>
-                    <select id="decade" value={selectedDecade} onChange={(e) => setSelectedDecade(e.target.value)}>
-                        <option value="70s">70s</option>
-                        <option value="80s">80s</option>
-                        <option value="90s">90s</option>
-                    </select>
+                <Stack direction="row" justifyContent="center">
+                    <Chip label={"70s"} color={'warning'} onClick={handleOnClick.bind(this, "70s")}/>
+                    <Chip label={"80s"} color={'warning'} onClick={handleOnClick.bind(this, "80s")}/>
+                    <Chip label={"90s"} color={'warning'} onClick={handleOnClick.bind(this, "90s")}/>
+                </Stack>
+                <Box component="img"
+                     sx={{
+                         height: 233,
+                         width: 350,
+                     }}
+                     alt="Template image"
+                     src={tracks[currentTrack] && tracks[currentTrack].image[2]["#text"]}/>
+                {tracks.length > 0 &&
+                    <Box><Typography variant={"h3"}>{tracks[currentTrack].name}</Typography>
+                        <Typography
+                            variant={"h4"}>{tracks[currentTrack].artist.name}</Typography></Box>
+    }
+                <Stack direction="row" justifyContent="center">
+                    <Button><SkipPreviousRounded onClick={handlePreviousClick}/></Button>
+                    <Button><PlayArrow onClick={handlePlayClick}/></Button>
+                    <Button><SkipNextRounded onClick={handleNextClick}/></Button>
+                </Stack>
+                <div className="video-frame">
+                    <iframe id="audioPlayer" width="560" height="315" src={currentYoutubeURL}
+                            frameBorder="0" allow="autoplay; encrypted-media"
+                            allowFullScreen ></iframe>
                 </div>
-                <ul>
-                    <Box>
-                        <h1>Youtube video Play/Stop</h1>
-
-                        <div className="video-frame">
-                            <iframe id="youtubeIframe" width="560" height="315" src={currentYoutubeURL}
-                                    frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
-                        </div>
-
-                        <button id="play">Play</button>
-                        <button id="stop">Stop</button>
-                    </Box>
-
-                    {tracks.map((track: TrackObject) => (
-                        <li key={track.name}>
-                            {track.name} - {track.artist.name}
-                            <button onClick={() => playTrack(track)}>Play</button>
-                        </li>
-                    ))}
-                </ul>
-                {currentTrack && (
-                    <audio controls autoPlay>
-                        <source src={currentTrack} type="audio/mpeg"/>
-                        Your browser does not support the audio element.
-                    </audio>
-                )}
             </div>
         );
     }
